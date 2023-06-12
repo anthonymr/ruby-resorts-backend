e12e0ca5f01e1bd780d8b6ed32133b2770e9613c
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-06-12 13:01:29 +0100

    Fix rubocop

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,7 +1,7 @@
 class Hotel < ApplicationRecord
-    has_many :rooms, dependent: :destroy
-    has_many :users, through: :bookings
-    
-    validates :name, presence: true
-    validates :city, presence: true
+  has_many :rooms, dependent: :destroy
+  has_many :users, through: :bookings
+
+  validates :name, presence: true
+  validates :city, presence: true
 end
