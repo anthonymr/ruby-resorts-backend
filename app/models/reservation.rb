216aa3fb513ecc3a4fbb28da07efc868a4c7ed14
Anthony Martin <anthonymartin20@gmail.com>
2023-06-13 04:49:23 -0400

    Fix isue calculating new reservation amount

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,6 @@ class Reservation < ApplicationRecord
   belongs_to :room
 
   def calculate_amount
-    (end_date - start_date).to_i * room.reservation_price
+    (end_date - start_date).to_i * Room.find(room_id).reservation_price
   end
 end
