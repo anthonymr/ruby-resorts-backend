96965c21b20445810c6713bd078a480231cc1eb2
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-06-13 11:31:16 +0100

    Fix rubocop error

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,7 +7,7 @@ class Reservation < ApplicationRecord
   validates :end_date, presence: true
   validates :room_id, presence: true
   validates :user_id, presence: true
-  
+
 
   def calculate_amount
     (end_date - start_date).to_i * Room.find(room_id).reservation_price
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
   before(:each) do
-    @room = @room = Room.new(name: 'Example room', description: 'Example description', full_price: 100, reservation_price: 10,
-      reservation_fee: 10, rating: 5)
+    @room = @room = Room.new(name: 'Example room', description: 'Example description',
+                             full_price: 100, reservation_price: 10,
+                             reservation_fee: 10, rating: 5)
     @room.image.attach(
-    io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'test.png')),
-    filename: 'test.png',
-    content_type: 'image/png'
+      io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'test.png')),
+      filename: 'test.png',
+      content_type: 'image/png'
     )
     @room.save
     @user = User.create(name: 'Example User', username: 'username', email: 'test@test.com', password: 'password')
     @hotel = Hotel.create(name: 'Example hotel', city: 'Example city')
-    @reservation = Reservation.new(start_date: Date.today, end_date: Date.tomorrow, room_id: @room.id, user_id: @user.id, hotel_id: @hotel.id)
+    @reservation = Reservation.new(start_date: Date.today, end_date: Date.tomorrow, room_id: @room.id,
+                                   user_id: @user.id, hotel_id: @hotel.id)
   end
 
   it 'is valid with valid attributes' do
