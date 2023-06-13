dfd7d7a3dac7eef0cb80be97329605bbd5783525
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-06-13 09:09:02 +0100

    Add routes to reservation

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
       resources :users, only: %i[create destroy]
       patch 'users', to: 'users#update'
       resources :rooms, only: %i[create destroy show index]
-      resources :hotels, only: %i[:index]
+      resources :hotels, only: :index
+      resources :reservations, only: %i[index show create destory]
+
 
       post 'authentication', to: 'authentication#create'
       delete 'authentication', to: 'authentication#destroy'
