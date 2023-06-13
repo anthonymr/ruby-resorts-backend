1ac05acbbd9e5268c20895d20664e306502d1769
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-06-13 11:38:08 +0100

    Fix errors

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,7 +7,6 @@ class Reservation < ApplicationRecord
   validates :room_id, presence: true
   validates :user_id, presence: true
 
-
   def calculate_amount
     (end_date - start_date).to_i * Room.find(room_id).reservation_price
   end
