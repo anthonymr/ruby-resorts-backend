bd41d918c22422aea4adb213fecd97698524c91c
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-06-13 11:36:18 +0100

    Fix error

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,6 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :hotel
   belongs_to :room
-
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :room_id, presence: true
