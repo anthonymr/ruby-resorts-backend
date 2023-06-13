1b29de699f3124ceff557f65223469ee440eddef
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-06-13 11:23:55 +0100

    Add validation to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,6 +3,12 @@ class Reservation < ApplicationRecord
   belongs_to :hotel
   belongs_to :room
 
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :room_id, presence: true
+  validates :user_id, presence: true
+  
+
   def calculate_amount
     (end_date - start_date).to_i * Room.find(room_id).reservation_price
   end
