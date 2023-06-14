91427c466d64fb14f5274fb50f37717ad7d6f5df
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-06-14 13:31:28 +0100

    Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,31 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the bin/rails app:seed command (or created alongside the database with app:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user = User.new(name: 'Example User', username: 'username', email: 'test@test.com', password: 'password')
+user = User.new(name: 'Example User', username: 'admin', email: 'test@test.com', password: 'admin')
 user.update(role: 'admin')
 user.save
 
-hotel = Hotel.create(name: 'Hotel 1', city: 'City 1')
+Hotel.create(name:'Ruby Amsterdam',city:'Amsterdam')
+Hotel.create(name:'Ruby Valencia',city:'Valencia')
+Hotel.create(name:'Ruby Vienna',city:'Vienna')
+Hotel.create(name:'Ruby Barcelona',city:'Barcelona')
+Hotel.create(name:'Ruby Marseille',city:'Marseille')
+Hotel.create(name:'Ruby Sevilla',city:'Sevilla')
+Hotel.create(name:'Ruby Sylt',city:'Sylt')
 
 ##first room
 room1 = Room.new(name: 'Ruby', description: 'Ruby Suites offer the best we at Ruby Resorts have to offer. The suites come with 5-star amenities',
     full_price: 100, reservation_price: 10,
     reservation_fee: 10, rating: 5)
 room1.image.attach(
-io: File.open(Rails.root.join('db', 'fixtures', 'files', 'room1.jpg')),
+io: File.open(Rails.root.join('app', 'assets', 'images', 'room1.jpg')),
 filename: 'room1.jpg',
-content_type: 'image/jpg'
+content_type: 'image/jpeg'
 )
 room1.save
 
@@ -28,9 +34,9 @@ room2 = Room.new(name: 'Emerald', description: 'Emerald Suites are second only t
     full_price: 200, reservation_price: 20,
     reservation_fee: 10, rating: 4)
 room2.image.attach(
-io: File.open(Rails.root.join('db', 'fixtures', 'files', 'room2.jpg')),
+io: File.open(Rails.root.join('app', 'assets', 'images', 'room2.jpg')),
 filename: 'room2.jpg',
-content_type: 'image/jpg'
+content_type: 'image/jpeg'
 )
 room2.save
 
@@ -39,9 +45,9 @@ room3 = Room.new(name: 'Diamond', description: 'Diamond Suites strike the right 
     full_price: 300, reservation_price: 10,
     reservation_fee: 10, rating: 4)
 room3.image.attach(
-io: File.open(Rails.root.join('db', 'fixtures', 'files', 'room3.jpg')),
+io: File.open(Rails.root.join('app', 'assets', 'images', 'room3.jpg')),
 filename: 'room3.jpg',
-content_type: 'image/jpg'
+content_type: 'image/jpeg'
 )
 room3.save
 
@@ -50,9 +56,9 @@ room4 = Room.new(name: 'Gold', description: 'Traveling for work or pleasure or b
     full_price: 100, reservation_price: 10,
     reservation_fee: 10, rating: 5)
 room4.image.attach(
-io: File.open(Rails.root.join('db', 'fixtures', 'files', 'room4.jpg')),
+io: File.open(Rails.root.join('app', 'assets', 'images', 'room4.jpg')),
 filename: 'room4.jpg',
-content_type: 'image/jpg'
+content_type: 'image/jpeg'
 )
 room4.save
 
@@ -62,16 +68,8 @@ room5 = Room.new(name: 'Silver', description: 'Silver Family Suites are well-equ
     full_price: 100, reservation_price: 10,
     reservation_fee: 10, rating: 5)
 room5.image.attach(
-io: File.open(Rails.root.join('db', 'fixtures', 'files', 'room5.jpg')),
+io: File.open(Rails.root.join('app', 'assets', 'images', 'room5.jpg')),
 filename: 'room5.jpg',
-content_type: 'image/jpg'
+content_type: 'image/jpeg'
 )
 room5.save
-
-reservation1 = Reservation.new(start_date: Date.today, end_date: Date.tomorrow, room_id: room1.id,
-    user_id: user.id, hotel_id: hotel.id)
-reservation1.save
-
-reservation2 = Reservation.new(start_date: Date.today, end_date: Date.tomorrow, room_id: room3.id,
-    user_id: user.id, hotel_id: hotel.id)
-reservation2.save
